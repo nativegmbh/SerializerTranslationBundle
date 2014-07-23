2dc5fa399af8fa2c98929e8cf8ea438f7a962526
Jérémy Jégou <jeremy.jegou@fr.airtag.com>
2014-07-23 14:33:50 +0200

    Init bundle
    Signed-off-by: Jérémy Jégou <jeremy.jegou@fr.airtag.com>

---
+++ AvooSerializerTranslationBundle.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Avoo\SerializerTranslationBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * {@inheritdoc}
+ */
+class AvooSerializerTranslationBundle extends Bundle
+{
+}
---
+++ DependencyInjection/Configuration.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Avoo\SerializerTranslationBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+class Configuration implements ConfigurationInterface
+{
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder();
+        $treeBuilder->root('avoo_serializer_translation');
+
+        return $treeBuilder;
+    }
+}
