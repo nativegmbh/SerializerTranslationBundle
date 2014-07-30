6a9371030fa12f649e5ebf638cf027c4f7302b0b
Jérémy Jégou <jeremy.jegou@fr.airtag.com>
2014-07-30 12:10:23 +0200

    Add expression language
    Signed-off-by: Jérémy Jégou <jeremy.jegou@fr.airtag.com>

---
+++ ExpressionLanguage/ExpressionLanguage.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Avoo\SerializerTranslationBundle\ExpressionLanguage;
+
+use Symfony\Component\ExpressionLanguage\ExpressionLanguage as BaseExpressionLanguage;
+
+/**
+ * @author Fabien Potencier <fabien@symfony.com>
+ */
+class ExpressionLanguage extends BaseExpressionLanguage
+{
+}
